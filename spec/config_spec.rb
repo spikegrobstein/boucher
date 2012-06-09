f7ce4cd3e8986ec05bc16ed8b5dec2de7c350480
Spike Grobstein <spikegrobstein@mac.com>
2012-06-09 18:15:59 -0400

    wrote some tests around env_for and role_for

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -5,20 +5,6 @@ describe Souce::Config do
 
   context "DSL" do
     let(:dsl) { Souce::Config::DSL.new }
-    let(:block) {
-      lambda do
-        user 'admin'
-
-        map_role :app, :application
-        map_role :web, :balancer
-
-        map_env :prod, :production
-
-        raw_system_address '10.3.2.100'
-
-        base_domain 'example.com'
-      end
-    }
 
     it "should set the user appropriately" do
       dsl.user 'spike'
@@ -80,44 +66,73 @@ describe Souce::Config do
 
   end
 
-  context "#env_for" do
+  context "maps" do
 
-  end
+    before(:all) do
+      Souce::Config.configure do
+        map_env :prod, 'production'
+        map_env :qa, 'preproduction'
 
-  context "#role_for" do
-
-  end
-
-  context "#get_map" do
-
-    let(:map) do
-      { :app => 'application', :db => 'postgres' }
+        map_role :app, 'rails'
+        map_role :web, 'balancer'
+      end
     end
 
-    context "with a symbol" do
+    context "#env_for" do
 
-      it "should return the right item from the map if it exists" do
-        Souce::Config.get_map(:app, map).should == 'application'
+      it "should map an env properly if it is configured" do
+        Souce::Config.env_for(:prod).should == 'production'
       end
 
-      it "should return the item if it does not exist in the map" do
-        Souce::Config.get_map(:spike, map).should == 'spike'
-      end
-
-    end
-
-    context "with a string" do
-
-      it "should return the right item from the map if it exists" do
-        Souce::Config.get_map('app', map).should == 'application'
-      end
-
-      it "should return the item if it does not exist in the map" do
-        Souce::Config.get_map('spike', map).should == 'spike'
+      it "should return the passed env if it's not configured" do
+        Souce::Config.env_for(:staging).should == 'staging'
       end
 
     end
 
+    context "#role_for" do
+
+      it "should map a role properly if it's been configured" do
+        Souce::Config.role_for(:app).should == 'rails'
+      end
+
+      it "should map a role properly if it hasn't been configured" do
+        Souce::Config.role_for(:db).should == 'db'
+      end
+
+    end
+
+    context "#get_map" do
+
+      let(:map) do
+        { :app => 'application', :db => 'postgres' }
+      end
+
+      context "with a symbol" do
+
+        it "should return the right item from the map if it exists" do
+          Souce::Config.get_map(:app, map).should == 'application'
+        end
+
+        it "should return the item if it does not exist in the map" do
+          Souce::Config.get_map(:spike, map).should == 'spike'
+        end
+
+      end
+
+      context "with a string" do
+
+        it "should return the right item from the map if it exists" do
+          Souce::Config.get_map('app', map).should == 'application'
+        end
+
+        it "should return the item if it does not exist in the map" do
+          Souce::Config.get_map('spike', map).should == 'spike'
+        end
+
+      end
+
+    end
   end
 
 end
