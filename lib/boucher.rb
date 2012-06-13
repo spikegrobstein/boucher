88cd27e0d33f0056d7812899e1c1d3b01913258d
Spike Grobstein <spikegrobstein@mac.com>
2012-06-13 00:31:37 -0400

    removed some unused code

--- lib/boucher.rb
+++ lib/boucher.rb
@@ -7,20 +7,5 @@ require 'boucher/capistrano'
 require 'pp'
 
 module Boucher
-  class << self
-
-    attr_accessor :cap
-
-    def bootstrap(hostname, role, &block)
-      puts "That boucher is DELISH!"
-
-      Config.configure(&block)
-
-      blueprint = Blueprint.new(hostname, role, Config)
-      @cap = Capistrano.new(blueprint)
-
-      @cap.run_recipe
-    end
-
-  end
+  # nothing to see here.
 end
